d0906c324e3b97b4f193bc73a009cae0584127de
leticia <nzbtm60@gmail.com>
2019-02-18 14:46:04 +0100

    ajout du nouvo s5

--- sprint/s5.js
+++ sprint/s5.js
@@ -4,8 +4,12 @@ const nb2 = prompt("le second nombre est");
 if (nb1< nb2) {
 	console.log(`${nb2}`);
 }
-else{
+else if (nb1>nb2) 
+{
 	console.log(`${nb1}`);
+}
+else{
+	console.log("ils sont egaux")
 }
 
 
