82f76f019a7dedb7072e8d7d3bf06e5a7bbfd2f4
leticia <nzbtm60@gmail.com>
2019-02-25 15:51:22 +0100

    ajout du s9

---
+++ sprint/s9.js
@@ -0,0 +1,9 @@
+let nb1 = prompt("le premier nombre est");
+let nb2 = prompt("le second nombre est");
+let nb3 = prompt("le troixieme nombre est");
+let nb4 = prompt("le quatrieme nombre est");
+let nb5 = prompt("le cinquieme nombre est");
+let superieur = Math.max(nb1, nb2, nb3, nb4, nb5);
+console.log(`le plus grand est: ${superieur}`);
+
+
